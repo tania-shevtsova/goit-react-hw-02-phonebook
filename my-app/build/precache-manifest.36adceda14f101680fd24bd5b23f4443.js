07cef46904d11b65811539461b0f0cf2753233ab
Tania <taniameoow@gmail.com>
2020-02-05 08:47:20 +0200

    deploy

---
+++ my-app/build/precache-manifest.36adceda14f101680fd24bd5b23f4443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514f747ecbd8e242189184049912574c",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c3d398e483b4e3b12b5d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.1f245cb4.chunk.css"
+  },
+  {
+    "revision": "4a8cb5fb4b3280bc5d4b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.57a85665.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.57a85665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d398e483b4e3b12b5d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.ff9e70f4.chunk.js"
+  },
+  {
+    "revision": "087bead6dde9c43ad19e",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.c604e018.js"
+  }
+]);
